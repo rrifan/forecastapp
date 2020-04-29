7a5062a5f85f3e0135afd7beaf3147b532a1982e
rifan <rifanripan@gmail.com>
2020-04-29 11:32:25 +0700

    first

---
+++ app/src/main/java/com/example/ForecastApp/activities/MapActivity.java
@@ -0,0 +1,101 @@
+package com.example.ForecastApp.activities;
+
+import android.annotation.SuppressLint;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.util.Log;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.ForecastApp.activities.BaseActivity;
+
+import com.example.ForecastApp.R;
+import com.example.ForecastApp.viewmodels.MapViewModel;
+import com.example.ForecastApp.utils.UI;
+
+public class MapActivity extends BaseActivity {
+
+    private WebView webView;
+    private MapViewModel mapViewModel;
+
+    @SuppressLint({"SetJavaScriptEnabled", "AddJavascriptInterface"})
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        setTheme(theme = UI.getTheme(prefs.getString("theme", "fresh")));
+        mapViewModel = ViewModelProviders.of(this).get(MapViewModel.class);
+
+        if (savedInstanceState == null) {
+            mapViewModel.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+            mapViewModel.mapLat = prefs.getFloat("latitude", 0);
+            mapViewModel.mapLon = prefs.getFloat("longitude", 0);
+            mapViewModel.apiKey = mapViewModel.sharedPreferences.getString("apiKey", getResources().getString(R.string.apiKey));
+        }
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/map.html?lat=" + mapViewModel.mapLat + "&lon="
+                + mapViewModel.mapLon + "&appid=" + mapViewModel.apiKey
+                + "&zoom=" + mapViewModel.mapZoom);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                if (savedInstanceState != null) {
+                    setMapState(mapViewModel.tabPosition);
+                }
+            }
+        });
+
+        BottomNavigationView bottomBar = findViewById(R.id.navigationBar);
+        bottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int i = item.getItemId();
+                setMapState(i);
+                mapViewModel.tabPosition = i;
+                return true;
+            }
+        });
+
+    }
+
+    private void setMapState(int item) {
+        switch (item) {
+            case R.id.map_clouds:
+                webView.loadUrl("javascript:map.removeLayer(rainLayer);map.removeLayer(windLayer);map.removeLayer(tempLayer);"
+                        + "map.addLayer(cloudsLayer);");
+                break;
+            case R.id.map_rain:
+                webView.loadUrl("javascript:map.removeLayer(cloudsLayer);map.removeLayer(windLayer);map.removeLayer(tempLayer);"
+                        + "map.addLayer(rainLayer);");
+                break;
+            case R.id.map_wind:
+                webView.loadUrl("javascript:map.removeLayer(cloudsLayer);map.removeLayer(rainLayer);map.removeLayer(tempLayer);"
+                        + "map.addLayer(windLayer);");
+                break;
+            case R.id.map_temperature:
+                webView.loadUrl("javascript:map.removeLayer(cloudsLayer);map.removeLayer(windLayer);map.removeLayer(rainLayer);"
+                        + "map.addLayer(tempLayer);");
+                break;
+            default:
+                Log.w("WeatherMap", "Layer not configured");
+                break;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+}
---
+++ app/src/main/java/com/example/ForecastApp/fragments/AboutDialogFragment.java
@@ -0,0 +1,41 @@
+package com.example.ForecastApp.fragments;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import com.example.ForecastApp.R;
+
+public class AboutDialogFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        String VersionName;
+        Context Cont = getContext();
+        try {
+            VersionName =  Cont.getPackageManager().getPackageInfo(Cont.getPackageName(),0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            VersionName = getString(R.string.about_unknown);
+        }
+        AlertDialog alertDialog = new AlertDialog.Builder(getActivity())
+                .setTitle(getText(R.string.app_name))
+                .setMessage(TextUtils.concat(VersionName, "\n\n",
+                        getText(R.string.about_description), "\n\n",
+                        getText(R.string.about_src), "\n\n",
+                        getText(R.string.about_issues), "\n\n",
+                        getText(R.string.about_data)))
+                .setPositiveButton(R.string.dialog_ok, null)
+                .create();
+        alertDialog.show();
+        ((TextView)alertDialog.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+        return alertDialog;
+    }
+}
